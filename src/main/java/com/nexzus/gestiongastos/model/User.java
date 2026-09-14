f758e79a0f385acbd3da8e1f42e502b0458217e0
mateo <munozomateo@gmail.com>
2026-09-14 12:54:03 -0500

    Se agrega la relación de expense a user

--- src/main/java/com/nexzus/gestiongastos/model/User.java
+++ src/main/java/com/nexzus/gestiongastos/model/User.java
@@ -1,6 +1,7 @@
 package com.nexzus.gestiongastos.model;
 
 import com.nexzus.gestiongastos.model.enums.AuthProvider;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,12 +9,15 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -44,4 +48,7 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private AuthProvider provider;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Expense> expenses = new LinkedHashSet<>();
 }
