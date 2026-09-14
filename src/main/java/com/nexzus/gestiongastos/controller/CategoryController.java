15525c9682f909fe696c57702d0ab6e33a1460ca
mateo <munozomateo@gmail.com>
2026-09-14 13:12:02 -0500

    Controlador completo para CategoryController

--- src/main/java/com/nexzus/gestiongastos/controller/CategoryController.java
+++ src/main/java/com/nexzus/gestiongastos/controller/CategoryController.java
@@ -5,12 +5,20 @@ import com.nexzus.gestiongastos.model.CategoryResponseDto;
 import com.nexzus.gestiongastos.service.abstraction.ICategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/category")
@@ -19,7 +27,29 @@ public class CategoryController {
     private final ICategoryService categoryService;
 
     @PostMapping
-    public ResponseEntity<CategoryResponseDto> create(@RequestBody @Valid CreateCategoryDto request){
+    public ResponseEntity<CategoryResponseDto> create(@RequestBody @Valid CreateCategoryDto request) {
         return new ResponseEntity<>(categoryService.create(request), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponseDto> getById(@PathVariable UUID id) {
+        return new ResponseEntity<>(categoryService.getById(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CategoryResponseDto>> getAll(Pageable pageable) {
+        return new ResponseEntity<>(categoryService.getAll(pageable), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        categoryService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponseDto> update(@RequestBody @Valid CreateCategoryDto request,
+                                                      @PathVariable UUID id) {
+        return new ResponseEntity<>(categoryService.updateById(id, request), HttpStatus.OK);
     }
 }
